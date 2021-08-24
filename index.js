26ac02a8235ba83dc86fde474c6ddc60e1c3361d
Tiago Correia de Lima <lima.tiagocorreia@gmail.com>
2021-08-24 00:16:45 -0300

    added signs template

--- index.js
+++ index.js
@@ -3,6 +3,118 @@ const app = express();
 
 app.use(express.json())
 
+app.get('/signs', (req, res) => {
+    [{
+            letter: "A",
+            url: ""
+        },
+        {
+            letter: "B",
+            url: ""
+        },,
+        {
+            letter: "C",
+            url: ""
+        },,
+        {
+            letter: "D",
+            url: ""
+        },,
+        {
+            letter: "E",
+            url: ""
+        },,
+        {
+            letter: "F",
+            url: ""
+        },,
+        {
+            letter: "G",
+            url: ""
+        },,
+        {
+            letter: "H",
+            url: ""
+        },,
+        {
+            letter: "I",
+            url: ""
+        },,
+        {
+            letter: "J",
+            url: ""
+        },,
+        {
+            letter: "K",
+            url: ""
+        },,
+        {
+            letter: "L",
+            url: ""
+        },,
+        {
+            letter: "M",
+            url: ""
+        },,
+        {
+            letter: "N",
+            url: ""
+        },,
+        {
+            letter: "O",
+            url: ""
+        },
+        {
+            letter: "P",
+            url: ""
+        },
+        {
+            letter: "Q",
+            url: ""
+        },
+        {
+            letter: "R",
+            url: ""
+        },
+        {
+            letter: "S",
+            url: ""
+        },
+        {
+            letter: "T",
+            url: ""
+        },
+        {
+            letter: "U",
+            url: ""
+        },
+        {
+            letter: "V",
+            url: ""
+        },
+        {
+            letter: "W",
+            url: ""
+        },
+        {
+            letter: "X",
+            url: ""
+        },
+        {
+            letter: "Y",
+            url: ""
+        },
+        {
+            letter: "Z",
+            url: ""
+        },
+    ]
+})
+
+app.post('/signs', (req, res) => {
+
+})
+
 app.get('/tshirt', (req, res) => {
     res.status(200).send({
         tshirt: 'Camiseta',
@@ -12,12 +124,8 @@ app.get('/tshirt', (req, res) => {
 
 app.post('/tshirt/:id', (req, res) => {
 
-    const {
-        id
-    } = req.params;
-    const {
-        logo
-    } = req.body;
+    const id = req.params;
+    const logo = req.body;
 
     if (!logo) {
         res.status(418).send({
